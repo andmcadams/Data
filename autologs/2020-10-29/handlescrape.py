8109aa354ba95eeae1d267b7f1a97f8f94e95d61
Andrew McAdams <andmcadams@gmail.com>
2020-10-29 20:16:25 -0600

    AUTOCOMMIT---Add new data 2020-10-29

---
+++ autologs/2020-10-29/handlescrape.py
@@ -0,0 +1,32 @@
+import json
+
+logscrape = open('zulrahlogscrape.txt', 'r').read().split('\n')
+
+pages = {}
+
+for line in logscrape:
+		if line:
+			lineData = json.loads(line)
+
+			# For each loaded line, split into keys and assign each one to the table
+			for k in lineData.keys():
+				pages[k] = lineData[k][0]
+
+lastPage = 0
+lastCount = 0
+totalMaxDiff = 0
+for page in sorted(pages.keys(), key=lambda kv: int(kv)):
+	if pages[page] is None:
+		print('Page is null: {}'.format(page))
+		continue
+	firstCount = pages[page][0]
+	newLastCount = pages[page][-1]
+	maxDiff = 25*(int(page)-int(lastPage) - 1)*(lastCount-firstCount)
+	if maxDiff != 0:
+		print('Difference of {} between pages {} and {}. Max difference: {}'.format((lastCount-firstCount), lastPage, page, maxDiff))
+		totalMaxDiff += maxDiff
+
+	lastPage = page
+	lastCount = newLastCount
+
+print(totalMaxDiff)
